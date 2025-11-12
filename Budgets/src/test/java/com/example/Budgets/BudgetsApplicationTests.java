dab1bec94558f0c3dff16e3b207a6be8223a3207
Vincent Wong <vincentwong5609@gmail.com>
2025-11-11 22:04:08 -0800

    Project setup

---
+++ Budgets/src/test/java/com/example/Budgets/BudgetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Budgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
