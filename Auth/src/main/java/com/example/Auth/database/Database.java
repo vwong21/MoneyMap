a0d59bce4440807bc170def6a87186519d73c83c
Vincent Wong <vincentwong5609@gmail.com>
2025-12-06 23:41:06 -0800

    database setup

--- Auth/src/main/java/com/example/Auth/database/Database.java
+++ Auth/src/main/java/com/example/Auth/database/Database.java
@@ -1,5 +1,28 @@
 package com.example.Auth.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Database {
+
+    private final String url;
+    private final String user;
+    private final String password;
+
+    public Database() {
+        url = "jdbc:postgresql://127.0.0.1:5432/postgres";
+        user = "postgres";
+        password = "7363";
+    }
+
+    public Connection getConnection() throws SQLException {
+        final Properties props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", password);
+
+        return DriverManager.getConnection(url, props);
+    }
     
 }
