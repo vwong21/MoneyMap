e2cc861198c96b531520958b038664b319fe1233
Vincent Wong <vincentwong5609@gmail.com>
2026-01-31 12:22:46 -0800

    fixed exception handling

--- Auth/src/main/java/com/example/Auth/api/controller/AuthController.java
+++ Auth/src/main/java/com/example/Auth/api/controller/AuthController.java
@@ -31,12 +31,9 @@ public class AuthController {
  
     @PostMapping("/auth/register")
     public ResponseEntity<?> register(@RequestBody User user) {
-        try {
-            service.register(user);
-            return ResponseEntity.ok(user);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", e.getMessage()));
-        }
+        service.register(user);
+        return ResponseEntity.ok(user);
+
     }
 
     @PostMapping("/auth/login")
---
+++ Auth/src/main/java/com/example/Auth/exception/CustomerExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.example.Auth.exception;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomerExceptionHandler {
+    
+    @ExceptionHandler(value = ExistingUserException.class)
+    public ResponseEntity<?> handleExistingUserException(ExistingUserException exception, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("TimeStamp", new Date());
+        body.put("Message", exception.getMessage());
+        body.put("Path", request.getDescription(false)); 
+
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = InvalidRequestException.class)
+    public ResponseEntity<?> handleInvalidRequestException(InvalidRequestException exception, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("TimeStamp", new Date());
+        body.put("Message", exception.getMessage());
+        body.put("Path", request.getDescription(false)); 
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
