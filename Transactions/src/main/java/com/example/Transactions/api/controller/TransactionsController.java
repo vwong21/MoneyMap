c1ce9c69cf703672a9f807fe15abb3ddff2c32aa
Vincent Wong <vincentwong5609@gmail.com>
2026-02-08 19:37:01 -0800

    transaction controller for createTransaction done

--- Transactions/src/main/java/com/example/Transactions/api/controller/TransactionsController.java
+++ Transactions/src/main/java/com/example/Transactions/api/controller/TransactionsController.java
@@ -1,14 +1,30 @@
 package com.example.Transactions.api.controller;
 
+
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Transactions.api.dto.CreateTransactionRequest;
+import com.example.Transactions.api.model.Transaction;
+import com.example.Transactions.service.TransactionsService;
 
 @RestController
 public class TransactionsController {
+    private final TransactionsService service;
+
+    public TransactionsController(TransactionsService service) {
+        this.service = service;
+    }
     
     @PostMapping("/transactions")
-    public ResponseEntity<?> createTransaction() {
-        
+    public ResponseEntity<Transaction> createTransaction(@RequestBody CreateTransactionRequest request, Authentication authentication) {
+        UUID userID = (UUID) authentication.getPrincipal();
+        Transaction transaction = service.CreateTransaction(userID, request.getAmount(), request.getDescription(), request.getCategoryId());
+        return ResponseEntity.ok(transaction);
     }
 }
