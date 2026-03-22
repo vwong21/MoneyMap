00a62871cfafcf0ae5bedc94aaf11454bc11b8d8
Vincent Wong <vincentwong5609@gmail.com>
2026-03-21 23:40:56 -0700

    added exception handler for illegal argument

--- Transactions/src/main/java/com/example/Transactions/exception/CustomExceptionHandler.java
+++ Transactions/src/main/java/com/example/Transactions/exception/CustomExceptionHandler.java
@@ -1,8 +1,25 @@
 package com.example.Transactions.exception;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+    @ExceptionHandler(value = IllegalArgumentException.class);
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException exception, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("TimeStamp", new Date());
+        body.put("Message", exception.getMessage());
+        body.put("Path", request.getDescription(false));
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 }
